2bbc193f37c820d65fe2bbdc64023118569c9f9e
wook <gus5427@naver.com>
2022-09-01 14:38:42 +0900

    ADD : add user.entity.ts

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -9,32 +9,42 @@ import {
 } from "typeorm";
 
 import * as bcrypt from "bcrypt";
+import { Field, Int, ObjectType } from "@nestjs/graphql";
 
 @Entity()
+@ObjectType()
 export class User {
   @PrimaryGeneratedColumn()
+  @Field((type) => Int)
   user_id: number;
 
   @CreateDateColumn()
+  @Field()
   created_at: Date;
 
   @UpdateDateColumn()
+  @Field()
   updated_at: Date;
 
   @Column()
+  @Field({ description: "로그인 시 아이디" })
   login_id: string;
 
   @Column({ select: false })
+  @Field()
   password: string;
 
   @Column()
+  @Field()
   phone_number: string;
 
   @Column()
+  @Field()
   email: string;
 
   @Column({ select: false, nullable: true })
-  refreshToken: string;
+  @Field({ nullable: true, description: "jwt Refresh Token, 로그인 시 발급됨" })
+  refreshToken?: string;
 
   @BeforeInsert()
   @BeforeUpdate()
